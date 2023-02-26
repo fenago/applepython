0f80387081cb6e7b05b56ea77601bd823bd6628f
Shafiqphd <122993639+Shafiqphd@users.noreply.github.com>
2023-02-26 03:43:20 -0500

    Delete Data Analysis by using Pandas/Data Preparation/Duplicates Lab/Convert Data Types directory

--- Data Analysis by using Pandas/Data Preparation/Duplicates Lab/Convert Data Types/Solutions.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-df = pd.read_csv('abalone.csv')
-print(df.dtypes)
-df['Rings'] = df['Rings'].astype(float)
-dtypes = {'Sex': 'category'}
-df = df.astype(dtypes)
-print(df.dtypes)
